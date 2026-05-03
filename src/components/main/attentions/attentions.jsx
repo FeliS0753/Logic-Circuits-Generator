e7cfc28a5c501f442b02157894740f439829ae5b
FeliS210 <w.o0321@icloud.com>
2026-05-03 10:48:15 +0900

    Fix LaTeX escape

--- src/components/main/attentions/attentions.jsx
+++ src/components/main/attentions/attentions.jsx
@@ -12,7 +12,7 @@ export default function Attentions(){
                 <li><code className="latex-command">\cdot</code>,
                     <code className="latex-command">\oplus</code>の後に論理変数を入れる場合は空白を入れてください。
                 </li>
-                <li>外部から<InlineMath math={"\\LaTeX"}/>の数式入力をペーストする際には()に
+                <li>外部から<InlineMath math={`\\LaTeX`}/>の数式入力をペーストする際には()に
                     <code className="latex-command">\left</code>,
                     <code className="latex-command">\right</code>をつけないでください。
                 </li>
